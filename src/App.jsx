4d8be7682cb7f994455a65f2b35ba6a3389dcefc
daichi takagishi <solar.eeclipse@gmail.com>
2022-07-14 02:30:27 +0900

    途中保存

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-/* eslint react-hooks/exhaustive-deps: off*/
-import React, { useEffect, useState } from "react";
-import { ColorfulMessage } from "./components/ColorfulMessage";
-const App = () => {
-  //console.log("最初");
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(false);
-  const onClickCountup = () => {
-    setNum(num + 1);
-  };
+import React from "react";
+import "./styles.css";
 
-  const onClickSwitchShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  useEffect(() => {
-    //console.log("useeffect");
-    if (num > 0) {
-      if (num % 3 === 0) {
-        faceShowFlag || setFaceShowFlag(true);
-      } else {
-        faceShowFlag && setFaceShowFlag(false);
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [num]);
-
+export const App = () => {
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickCountup}>カウントアップ</button>
-      <br />
-      <button onClick={onClickSwitchShowFlag}>on/off</button>
-      <p>{num}</p>
-      {faceShowFlag && <p>😃</p>}
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>ああああああ</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p>いいいいいい</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>うううううう</p>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p>ええええええ</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
-export default App;
